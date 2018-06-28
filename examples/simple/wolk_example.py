d7b9db9a588252842abfe3ac3c265ceb826af844
Danilo Ješić <34022788+djesic@users.noreply.github.com>
2018-06-28 16:21:02 +0200

    Added exceptions for MQTT connection's return codes (#23)
    * Added exceptions for MQTT connection's return codes
    
    * Documentation for exceptions

--- examples/simple/wolk_example.py
+++ examples/simple/wolk_example.py
@@ -35,7 +35,11 @@ def main():
 
     # Establish a connection to the WolkAbout IoT Platform
     print("Connecting to WolkAbout IoT Platform")
-    wolk_device.connect()
+    try:
+        wolk_device.connect()
+    except RuntimeError as e:
+        print(str(e))
+        sys.exit(1)
 
     publish_period_seconds = 5
 
