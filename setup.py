d34ec06b08f44b4ff94f85690457b447b2a73473
Danilo Ješić <34022788+djesic@users.noreply.github.com>
2018-12-17 13:52:05 +0100

    Fixed escaping double quotes (#38)
    * Fixed escaping double quotes
    
    * Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wolk-connect",
-    version="3.1.1",
+    version="3.1.2",
     install_requires=["paho_mqtt==1.4.0", "python-persistent-queue==1.3.0"],
     include_package_data=True,
     license="Apache License 2.0",
