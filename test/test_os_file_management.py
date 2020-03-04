324c0d42105755f2b0fcc76de17d1c509db9e603
djesic <danilo.jesic@wolkabout.com>
2020-03-04 10:09:32 +0100

    Tests for file management

--- test/test_os_file_management.py
+++ test/test_os_file_management.py
@@ -12,13 +12,13 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import json
+import hashlib
 import logging
 import os
 import sys
-import time
 import unittest
 from tempfile import NamedTemporaryFile
+from threading import Timer
 from unittest.mock import MagicMock
 
 sys.path.append("..")  # noqa
@@ -289,7 +289,7 @@ class TestOSFileManagement(unittest.TestCase):
         self.assertIsNone(file_management.temp_file)
 
     def test_file_package_binary_idle_state(self):
-        """Test receveiving file package when in idle state."""
+        """Test receiving file package when in idle state."""
         mock_status_callback = MagicMock(return_value=None)
         mock_packet_request_callback = MagicMock(return_value=None)
         mock_url_status_callback = MagicMock(return_value=None)
@@ -307,3 +307,231 @@ class TestOSFileManagement(unittest.TestCase):
         file_management.handle_file_binary_response(file_transfer_package)
 
         file_management.logger.warning.assert_called_once()
+
+    def test_file_package_binary_cancel_timeout(self):
+        """Test receiving file package cancels request timeout timer."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_management.current_status = True
+        file_management.retry_count = 0
+        file_management.request_timeout = Timer(60.0, file_management._timeout)
+        file_management.request_timeout.cancel = MagicMock()
+
+        file_transfer_package = FileTransferPackage(b"", b"", b"")
+
+        file_management.handle_file_binary_response(file_transfer_package)
+        file_management.request_timeout.cancel()
+        file_management.packet_request_callback.assert_called()
+
+    # def test_file_package_binary(self):
+    #     """Test receiving file package cancels request timeout timer."""
+    #     mock_status_callback = MagicMock(return_value=None)
+    #     mock_packet_request_callback = MagicMock(return_value=None)
+    #     mock_url_status_callback = MagicMock(return_value=None)
+
+    #     file_management = OSFileManagement(
+    #         mock_status_callback,
+    #         mock_packet_request_callback,
+    #         mock_url_status_callback,
+    #     )
+    #     file_management.logger.setLevel(logging.CRITICAL)
+    #     file_management.current_status = True
+    #     file_management.retry_count = 0
+    #     data = b"Let's try something else"
+    #     current_hash = hashlib.sha256(data).digest()
+
+    #     hashlib.sha256 = MagicMock(return_value=b"")
+
+    #     file_transfer_package = FileTransferPackage(
+    #         32 * b"\x00", data, current_hash
+    #     )
+
+    #     file_management.handle_file_binary_response(file_transfer_package)
+    #     file_management.request_timeout.cancel()
+    #     file_management.packet_request_callback.assert_called()
+
+    def test_handle_file_url_download_abort(self):
+        """Test method resets state."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_management.handle_file_url_download_abort()
+        self.assertIsNone(file_management.current_status)
+
+    def test_get_file_list_current_dir(self):
+        """Test get file list for running in current directory."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_list = file_management.get_file_list()
+        self.assertNotEqual(0, len(file_list))
+
+    def test_get_file_list_empty_dir(self):
+        """Test get file list for running in current directory."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        os.mkdir("test_dir")
+        file_management.file_directory = "test_dir"
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_list = file_management.get_file_list()
+        self.assertEqual(0, len(file_list))
+        os.rmdir("test_dir")
+
+    def test_get_file_path_existing(self):
+        """Test get file path for existing file."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_name = "test_file"
+        file_handle = open(file_name, "w")
+        file_handle.close()
+        file_path = file_management.get_file_path(file_name)
+        expected_file_path = os.path.join(
+            os.path.abspath(os.getcwd()), file_name
+        )
+        self.assertEqual(expected_file_path, file_path)
+        os.remove(file_name)
+
+    def test_get_file_path_non_existing(self):
+        """Test get file path for non_existing file."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_name = "test_file"
+        file_path = file_management.get_file_path(file_name)
+        self.assertIsNone(file_path)
+
+    def handle_file_list_confirm_does_nothing(self):
+        """Test file list confirm doesn't call status callbacks."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_management.handle_file_list_confirm()
+        file_management.status_callback.assert_not_called()
+
+    def test_handle_file_delete_non_existing(self):
+        """Test deleting file that doesn't exist."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_name = "test_file"
+        file_management.handle_file_delete(file_name)
+        self.assertFalse(os.path.exists(os.path.join(os.getcwd(), file_name)))
+
+    def test_handle_file_delete_existing(self):
+        """Test deleting file that exists."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_name = "test_file"
+        file_handle = open(file_name, "w")
+        file_handle.close()
+        file_management.handle_file_delete(file_name)
+        self.assertFalse(os.path.exists(os.path.join(os.getcwd(), file_name)))
+
+    def test_handle_file_purge(self):
+        """Test deleting all regular files in a directory."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_names = ["file1", "file2", ".special-file"]
+        files_directory = "test_dir"
+        os.mkdir(files_directory)
+        directory_path = os.path.join(os.getcwd(), files_directory)
+        for file in file_names:
+            file_handle = open(os.path.join(directory_path, file), "w")
+            file_handle.close()
+
+        file_management.file_directory = directory_path
+        file_management.handle_file_purge()
+        self.assertEqual(1, len(os.listdir(directory_path)))
+        os.remove(os.path.join(directory_path, ".special-file"))
+        os.rmdir(files_directory)
+
+    def test_timeout(self):
+        """Test timeout calls abort."""
+        mock_status_callback = MagicMock(return_value=None)
+        mock_packet_request_callback = MagicMock(return_value=None)
+        mock_url_status_callback = MagicMock(return_value=None)
+
+        file_management = OSFileManagement(
+            mock_status_callback,
+            mock_packet_request_callback,
+            mock_url_status_callback,
+        )
+        file_management.logger.setLevel(logging.CRITICAL)
+        file_management.handle_file_upload_abort = MagicMock()
+
+        file_management._timeout()
+        file_management.handle_file_upload_abort.assert_called_once()
